95af9d29ec3577950157e2f81a1dc1f9b2511150
zitongliu <zitongliu7@gmail.com>
2017-10-22 16:51:50 +1100

    handled non async actions in middleware

--- src/middlewares/async.js
+++ src/middlewares/async.js
@@ -1,8 +1,12 @@
 export default function({ dispatch }) {
   return next => action => {
-    console.log(action);
+    // if there is not a payload or the payload does not have a .then property. Just send it forward to next middleware
+    // this means the action is not an async action
+    if (!action.payload || !action.payload.then) {
+      // next sends the action to the next middleware or reducer (if no other middleware)
+      return next(action)
+    }
 
-    // next sends the action to the next middleware or reducer (if no other middleware)
-    next(action);
+    console.log('we dont have a promise', action);
   };
 }
